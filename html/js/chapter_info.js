68487196449530831d47f48da8d5f00a60a2d08f
Eric McGregor <eric@n0code.net>
2024-05-04 17:35:20 -0400

    added chapter html files

---
+++ html/js/chapter_info.js
@@ -0,0 +1,362 @@
+var chapterData = [
+  {
+    "number": 0,
+    "id": "00_01_introduction",
+    "title": "Introduction",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 1,
+    "id": "01_01_creating_selections",
+    "title": "Creating Selection",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 1,
+    "id": "01_02_selection_node_properties",
+    "title": "Selection and Node Properties",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 1,
+    "id": "01_03_modifying_elements",
+    "title": "Modifying Elements in Selections",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 1,
+    "id": "01_04_create_delete_move_elements",
+    "title": "Creating, Deleting, and Moving Elements",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 1,
+    "id": "01_05_selection_call",
+    "title": "Selection.call",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 2,
+    "id": "02_01_binding_data",
+    "title": "Binding Data to Elements",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 2,
+    "id": "02_02_binding_object_data",
+    "title": "Binding Object Data",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 2,
+    "id": "02_03_modifying_data",
+    "title": "Modifying Data",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 2,
+    "id": "02_04_reordering_elements",
+    "title": "Reordering Elements",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 3,
+    "id": "03_01_d3_fetch",
+    "title": "Using D3-fetch",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 4,
+    "id": "04_01_scaling_functions",
+    "title": "Scaling Functions",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 4,
+    "id": "04_02_continuous_scales",
+    "title": "Continuous Scales",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 4,
+    "id": "04_03_axis",
+    "title": "Axis",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 4,
+    "id": "04_04_working_with_dates",
+    "title": "Working With Dates",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 4,
+    "id": "04_05_sequential_scales",
+    "title": "Sequential Scales",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 4,
+    "id": "04_06_quantize_scales",
+    "title": "Quantize Scales",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 4,
+    "id": "04_07_ordinal_scales",
+    "title": "Ordinal Scales",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 4,
+    "id": "04_08_legends",
+    "title": "Legends",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 5,
+    "id": "05_01_paths",
+    "title": "Paths",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 5,
+    "id": "05_02_lines",
+    "title": "Lines",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 5,
+    "id": "05_03_radial_lines",
+    "title": "Radial Lines",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 5,
+    "id": "05_04_curves",
+    "title": "Curves",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 5,
+    "id": "05_05_areas",
+    "title": "Areas and Area Radials",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 5,
+    "id": "05_06_stacks",
+    "title": "Stacks",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 5,
+    "id": "05_07_arcs_pie_charts",
+    "title": "Arcs and Pie Charts",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 5,
+    "id": "05_08_links",
+    "title": "Links",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 5,
+    "id": "05_09_polygons",
+    "title": "Polygon",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 5,
+    "id": "05_10_symbols",
+    "title": "Symbols",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 6,
+    "id": "06_01_hierarchal",
+    "title": "Hierarchal Models",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 6,
+    "id": "06_02_trees",
+    "title": "Tree, Cluster, and Radial Layouts",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 6,
+    "id": "06_03_treemaps",
+    "title": "Treemaps",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 6,
+    "id": "06_04_partitions",
+    "title": "Partition Layouts",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 6,
+    "id": "06_05_packs",
+    "title": "Circle-Packing Layouts",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 7,
+    "id": "07_01_word_clouds",
+    "title": "Word Clouds",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 7,
+    "id": "07_02_chords",
+    "title": "Chord Layout",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 8,
+    "id": "08_01_events",
+    "title": "Events",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 8,
+    "id": "08_02_dispatches",
+    "title": "Dispatches",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 8,
+    "id": "08_03_require",
+    "title": "Require",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 8,
+    "id": "08_04_timers",
+    "title": "Timers",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 9,
+    "id": "09_01_transitions",
+    "title": "Transitions",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 9,
+    "id": "09_02_easings",
+    "title": "Easings",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 9,
+    "id": "09_03_brushes",
+    "title": "Brushes",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 9,
+    "id": "09_04_zooming",
+    "title": "Zooming",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  },
+  {
+    "number": 9,
+    "id": "09_05_dragging",
+    "title": "Dragging",
+    "start_code": "",
+    "exercises": [],
+    "include": null
+  }
+];
